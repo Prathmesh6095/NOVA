9cd5ccb2e4bd0fd909ef76e443518e197718e489
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-18 11:29:51 +0530

    Some adjustments done

--- NOVA.py
+++ NOVA.py
@@ -143,7 +143,7 @@ def start():
             os.system("taskkill /f /im EXCELS.EXE")
             speak("Done sir.")
 
-        elif "take screenshot" in query or "take a screenshot" in query:
+        elif "take screenshot " in query or "take a screenshot" in query:
             speak("sir,please tell me the name for this screenshot file.")
             name = takecommand().lower()
             speak("please sir hold the screenfor few seconds, i am taking screenshot")
@@ -388,15 +388,15 @@ def start():
             elif "leave it for now" in query or "leave it" in query:
                 speak("Okay sir..")
 # ----------To open apps from pc ----------#
-            elif "open" in query:
-                from Dictapp import openweb
-                openweb(query)
-                speak("Done sir.")
+            # elif "open" in query:
+            #     from Dictapp import openweb
+            #     openweb(query)
+            #     speak("Done sir.")
             
-            elif "close" in query:
-                from Dictapp import closeappweb
-                closeappweb(query)
-                speak("Done sir.")
+            # elif "close" in query:
+            #     from Dictapp import closeappweb
+            #     closeappweb(query)
+            #     speak("Done sir.")
         #speak("Sir , do you have any other work?")
             
 #main function
