c89ab7d42710d02ba7c1870611e8424ca0150f12
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-02 21:03:10 +0530

    volume up command dunction added

--- keyboard.py
+++ keyboard.py
@@ -6,7 +6,7 @@ keyboard = Controller()
 
 def volumeup():
     for i in range(5):
-        keyboard.press(Key.media_volume_up)
+        keyboard.press(Key.media_volume_up )
         keyboard.release(Key.media_volume_up)
         sleep(0.1)
 
