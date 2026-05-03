d501af0643d85cef16aceb03c373dc07f1a948d2
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-03 12:40:34 +0530

    volume down command dunction added

--- keyboard.py
+++ keyboard.py
@@ -6,12 +6,12 @@ keyboard = Controller()
 
 def volumeup():
     for i in range(5):
-        keyboard.press(Key.media_volume_up )
+        keyboard.press(Key.media_volume_up)
         keyboard.release(Key.media_volume_up)
         sleep(0.1)
 
 def volumedown():
     for i in range(5):
-        keyboard.press(Key.media_volume_down)
+        keyboard.press(Key.media_volume_down )
         keyboard.release(Key.media_volume_down)
         sleep(0.1)
